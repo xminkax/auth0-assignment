128c180a972ad102c8d98c5330c14f510fb06f54
monikakindernayova <xminkax@gmail.com>
2018-12-03 22:08:09 +0100

    heroku

--- src/server.js
+++ src/server.js
@@ -18,32 +18,9 @@ const mapPlatform = ({fields}) => {
 const FAVOURITE_PLATFORM = 'favouritePlatform';
 const PLATFORM = 'platform';
 
-app.get('/favourite-platforms/:id', async (req, res) => {
-  try {
-    const response = await client.getEntries({
-      content_type: FAVOURITE_PLATFORM,
-      'fields.userId': req.params.id
-    });
-    const result = response.items[0].fields.platform.map(mapPlatform);
-    res.send(JSON.stringify(result));
-  } catch (error) {
-    res.status(500);
-    res.send(JSON.stringify({error}));
-  }
-});
-
 app.get('/platforms', async (req, res) => {
-    try {
-      const response = await client.getEntries({content_type: PLATFORM});
-      debugger;
-      const result = response.items.map(mapPlatform);
-      res.send(JSON.stringify(result));
-    } catch (error) {
-      res.status(500);
-      res.send(JSON.stringify({error}));
-    }
-  }
-);
+  res.send({});
+});
 
 app.listen(4000, () => {
   console.log('Node server listening on http://localhost:4000');
