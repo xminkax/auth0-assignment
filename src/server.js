f5cf56c2583aab6efbcb08a4f4a80a296ede6e5d
monikakindernayova <xminkax@gmail.com>
2018-12-03 22:02:15 +0100

    Add basic api calls

--- src/server.js
+++ src/server.js
@@ -1,19 +1,52 @@
 /* eslint-disable no-console */
 // Disabling 'no-console' as it's reasonable for this file to do some logging.
 const express = require('express');
+const contentful = require('contentful');
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
+const client = contentful.createClient({
+  space: 'rq9f9siz5b6e',
+  accessToken: '6806b20c28f79aecd70bb6001f46bc02063b527019835e1777db7fe8acdbb2ed'
 });
 
-app.get('/todo', (req, res) => {
-  res.send({});
+const mapPlatform = ({fields}) => {
+  const {title, imageName} = fields;
+  return {title, imageName}
+};
+
+const FAVOURITE_PLATFORM = 'favouritePlatform';
+const PLATFORM = 'platform';
+
+app.get('/favourite-platforms/:id', async (req, res) => {
+  try {
+    const response = await client.getEntries({
+      content_type: FAVOURITE_PLATFORM,
+      'fields.userId': req.params.id
+    });
+    const result = response.items[0].fields.platform.map(mapPlatform);
+    res.send(JSON.stringify(result));
+  } catch (error) {
+    res.status(500);
+    res.send(JSON.stringify({error}));
+  }
 });
 
-app.listen(4000, () => {
+app.get('/platforms', async (req, res) => {
+    try {
+      const response = await client.getEntries({content_type: PLATFORM});
+      debugger;
+      const result = response.items.map(mapPlatform);
+      res.send(JSON.stringify(result));
+    } catch (error) {
+      res.status(500);
+      res.send(JSON.stringify({error}));
+    }
+  }
+);
+
+app.listen(process.env.PORT || 4000, () => {
   console.log('Node server listening on http://localhost:4000');
 });
+
+
