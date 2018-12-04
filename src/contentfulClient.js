0e1ffc50a8ca577303e0232d13db423442c6e305
monikakindernayova <xminkax@gmail.com>
2018-12-04 15:31:56 +0100

    Add api calls

---
+++ src/contentfulClient.js
@@ -0,0 +1,115 @@
+/* eslint-disable no-console */
+// Disabling 'no-console' as it's reasonable for this file to do some logging.
+const contentful = require('contentful-management');
+
+const LOCALE = 'en-US';
+const SPACE_ID = 'rq9f9siz5b6e';
+const FAVOURITE_PLATFORM = 'favouritePlatform';
+const PLATFORM = 'platform';
+const ACCESS_TOKEN = 'CFPAT-566889042ed4f8f07944d66086b3b5dd2dde9600c45cdfc2cc3fb9d937dfc4d1';
+const ENVIRONMENT = 'master';
+
+const client = contentful.createClient({
+  accessToken: ACCESS_TOKEN,
+});
+
+const getEnvironment = async () => {
+  const space = await client.getSpace(SPACE_ID);
+  return space.getEnvironment(ENVIRONMENT);
+};
+
+const addUser = async (id) => {
+  const environment = await getEnvironment();
+  return environment.createEntry(FAVOURITE_PLATFORM, {
+    fields: {
+      userId: {
+        'en-US': id,
+      },
+    },
+  });
+};
+
+const addFavouritePlatform = async (userId, platformId) => {
+  const environment = await getEnvironment();
+  const entries = await environment.getEntries({
+    content_type: FAVOURITE_PLATFORM,
+    'fields.userId': userId,
+  });
+  const entry = entries.items[0];
+  const platforms = entry.fields.platform ? entry.fields.platform : entry.fields.platform = { 'en-US': [] };
+  platforms[LOCALE].push({
+    sys: {
+      id: platformId,
+      linkType: 'Entry',
+      type: 'Link',
+    },
+  });
+  entry.fields.platform = platforms;
+  return entry.update();
+};
+
+const deleteFavouritePlatform = async (userId, platformId) => {
+  const environment = await getEnvironment();
+  const entries = await environment.getEntries({
+    content_type: FAVOURITE_PLATFORM,
+    'fields.userId': userId,
+  });
+  const entry = entries.items[0];
+
+  if (!entry.fields.platform) {
+    return { message: 'Nothing to delete' };
+  }
+
+  const platforms = [];
+  entry.fields.platform[LOCALE].forEach((item) => {
+    if (item.sys.id !== platformId) {
+      platforms.push(item);
+    }
+  });
+
+  if (platforms.length === 0) {
+    delete entry.fields.platform;
+  } else {
+    entry.fields.platform[LOCALE] = platforms;
+  }
+
+  return entry.update();
+};
+
+const mapPlatform = ({ fields, sys }) => {
+  const { title, imageName } = fields;
+  const { id } = sys;
+  return { title: title[LOCALE], imageName: imageName[LOCALE], id };
+};
+
+const getFavouritePlatforms = async (userId) => {
+  const environment = await getEnvironment();
+  const entries = await environment.getEntries({
+    content_type: FAVOURITE_PLATFORM,
+    'fields.userId': userId,
+  });
+  const favouritePlatformsId = entries.items[0].fields.platform[LOCALE].map(({ sys }) => sys.id);
+
+  const favouritePlatforms = await environment.getEntries({
+    content_type: PLATFORM,
+    'sys.id[in]': favouritePlatformsId.join(','),
+  });
+  return favouritePlatforms.items.map(mapPlatform);
+};
+
+const getPlatforms = async () => {
+  const environment = await getEnvironment();
+  const entries = await environment.getEntries({
+    content_type: PLATFORM,
+  });
+  return entries.items.map(mapPlatform);
+};
+
+
+module.exports = {
+  addUser,
+  deleteFavouritePlatform,
+  addFavouritePlatform,
+  getFavouritePlatforms,
+  getPlatforms,
+};
--- src/server.js
+++ src/server.js
@@ -1,52 +1,40 @@
 /* eslint-disable no-console */
 // Disabling 'no-console' as it's reasonable for this file to do some logging.
 const express = require('express');
-const contentful = require('contentful');
+const contentfulClient = require('./contentfulClient');
 
 const app = express();
+app.use(express.json());
 
-const client = contentful.createClient({
-  space: 'rq9f9siz5b6e',
-  accessToken: '6806b20c28f79aecd70bb6001f46bc02063b527019835e1777db7fe8acdbb2ed'
+app.post('/user', async (req, res) => {
+  const entry = await contentfulClient.addUser(req.body.id);
+  res.send(JSON.stringify(entry));
 });
 
-const mapPlatform = ({fields}) => {
-  const {title, imageName} = fields;
-  return {title, imageName}
-};
+app.post('/favourite-platform', async (req, res) => {
+  const entry = await contentfulClient.addFavouritePlatform(req.body.userId, req.body.platformId);
+  res.send(JSON.stringify(entry));
+});
 
-const FAVOURITE_PLATFORM = 'favouritePlatform';
-const PLATFORM = 'platform';
+app.delete('/favourite-platform', async (req, res) => {
+  const entry = await contentfulClient.deleteFavouritePlatform(
+    req.body.userId,
+    req.body.platformId,
+  );
+  res.send(JSON.stringify(entry));
+});
 
-app.get('/favourite-platforms/:id', async (req, res) => {
-  try {
-    const response = await client.getEntries({
-      content_type: FAVOURITE_PLATFORM,
-      'fields.userId': req.params.id
-    });
-    const result = response.items[0].fields.platform.map(mapPlatform);
-    res.send(JSON.stringify(result));
-  } catch (error) {
-    res.status(500);
-    res.send(JSON.stringify({error}));
-  }
+app.get('/favourite-platforms/:userId', async (req, res) => {
+  const favouritePlatforms = await contentfulClient.getFavouritePlatforms(req.params.userId);
+  res.send(favouritePlatforms);
 });
 
 app.get('/platforms', async (req, res) => {
-    try {
-      const response = await client.getEntries({content_type: PLATFORM});
-      debugger;
-      const result = response.items.map(mapPlatform);
-      res.send(JSON.stringify(result));
-    } catch (error) {
-      res.status(500);
-      res.send(JSON.stringify({error}));
-    }
-  }
-);
+  const platforms = await contentfulClient.getPlatforms();
+  res.send(JSON.stringify(platforms));
+});
 
 app.listen(process.env.PORT || 4000, () => {
   console.log('Node server listening on http://localhost:4000');
 });
-
 
