f12fd19183e91dc35de0ea0a66df732a2f40b8d3
monikakindernayova <xminkax@gmail.com>
2018-12-07 10:06:17 +0000

    Start to style header and footer, for now just copy text from auth0

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import STYLES from './Header.scss';
 
 const Header = () => (
-  <div>Header</div>
+  <header className={STYLES.header}>
+    <div className={STYLES.container}>
+      <ul className={STYLES.menu}>
+        <li><span>Platform</span></li>
+        <li><span>Solutions</span></li>
+        <li><span>Why Auth0</span></li>
+        <li><span>Developers</span></li>
+        <li><span>Pricing</span></li>
+        <li><span>Talk To Sales</span></li>
+      </ul>
+    </div>
+  </header>
 );
 
 export default Header;
