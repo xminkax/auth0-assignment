54fb12ef7ec9a04d4fad9290c9a790c41fbad970
monikakindernayova <xminkax@gmail.com>
2018-12-11 15:11:51 +0100

    Server for heroku deploy

--- client/src/Auth/auth0-variables.js
+++
@@ -1,5 +0,0 @@
-export const AUTH_CONFIG = { // eslint-disable-line import/prefer-default-export
-  domain: 'minka.eu.auth0.com',
-  clientId: 'fy28an-jAzG1mrLQCAK_F7mabpuCqa4x',
-  callbackUrl: 'http://localhost:3000',
-};
