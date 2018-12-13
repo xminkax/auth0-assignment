dde89ea1c2208fe95c942b218503aba0b48d3f0f
monikakindernayova <xminkax@gmail.com>
2018-12-13 10:06:45 +0100

    Add node cache

--- server/src/routes/platforms.js
+++ server/src/routes/platforms.js
@@ -4,14 +4,26 @@ const { PLATFORM } = require('./contentTypes');
 
 const { LOCALE } = contentfulClient;
 
+const CacheService = require('../cache');
+
+const ttl = 60 * 60 * 1; // cache for 1 Hour
+const cache = new CacheService(ttl);
+
 const getPlatforms = async () => {
+  if (cache.get('platforms')) {
+    return cache.get('platforms');
+  }
+
   const environment = await contentfulClient.getEnvironment();
   const entries = await environment.getEntries({
     content_type: PLATFORM,
-    order:'fields.title'
+    order: 'fields.title',
   });
   const mapPlatformWithLocale = util.mapPlatform(LOCALE);
-  return entries.items.map(mapPlatformWithLocale);
+
+  const result = entries.items.map(mapPlatformWithLocale);
+  cache.set('platforms', result);
+  return result;
 };
 
 module.exports = {
